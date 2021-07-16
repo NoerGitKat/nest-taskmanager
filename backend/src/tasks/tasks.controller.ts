c71d831ca1016493a2174877b3f56ea45aa2a177
Noer Paanakker <noer@lightworksweb.nl>
2021-07-16 19:55:18 +0200

    create task dto + create task handler

--- backend/src/tasks/tasks.controller.ts
+++ backend/src/tasks/tasks.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { CreateTaskDto } from './create-task.dto';
+import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -6,7 +8,24 @@ export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks() {
+  getAllTasks(): Task[] {
     return this.tasksService.fetchTasks();
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Post()
+  async createNewTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
+    const createdTask = await this.tasksService.createTask(createTaskDto);
+    return createdTask;
+  }
+
+  @Delete('/:id')
+  async deleteTask(@Param('id') id: string): Promise<Task[]> {
+    const remainingTasks = await this.tasksService.deleteTask(id);
+    return remainingTasks;
   }
 }
--- backend/src/tasks/tasks.service.ts
+++ backend/src/tasks/tasks.service.ts
@@ -1,10 +1,43 @@
-import { Get, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDto } from './create-task.dto';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class TasksService {
-  private tasks = [];
+  private tasks: Task[] = [];
 
   public fetchTasks() {
     return this.tasks;
+  }
+
+  public getTaskById(id: string): Task {
+    const foundTask = this.tasks.find((task: Task) => task.id === id);
+    if (foundTask) {
+      return foundTask;
+    }
+  }
+
+  public createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+
+    const newTask = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(newTask);
+
+    return newTask;
+  }
+
+  public deleteTask(id: string): Task[] {
+    const filteredTasks = this.tasks.filter((task) => task.id !== id);
+
+    this.tasks = filteredTasks;
+
+    return filteredTasks;
   }
 }
